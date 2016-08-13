f8f13ab420d825c0c4a66797695604e4a312ec54
Luis Yucra <luis.yucra@gmail.com>
2016-08-13 08:26:35 -0500

    project skeleton

---
+++ server/src/main/java/com/milknekp/apps/utility/water/WaterUtilityManagmentApplication.java
@@ -0,0 +1,12 @@
+package com.milknekp.apps.utility.water;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterUtilityManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterUtilityManagmentApplication.class, args);
+	}
+}
---
+++ server/src/test/java/com/milknekp/apps/utility/water/WaterUtilityManagmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.milknekp.apps.utility.water;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WaterUtilityManagmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
