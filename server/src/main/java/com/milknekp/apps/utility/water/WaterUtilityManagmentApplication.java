ecfbb75fb7851fe0f66d3c644080604180dab1bc
Luis Yucra <luis.yucra@gmail.com>
2016-08-13 10:51:51 -0500

    added gitattributes

--- server/src/main/java/com/milknekp/apps/utility/water/WaterUtilityManagmentApplication.java
+++ server/src/main/java/com/milknekp/apps/utility/water/WaterUtilityManagmentApplication.java
@@ -1,10 +1,15 @@
 package com.milknekp.apps.utility.water;
 
+import com.milknekp.apps.utility.water.model.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WaterUtilityManagmentApplication {
+
+    @Autowired
+	private CustomerRepository customerRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaterUtilityManagmentApplication.class, args);
