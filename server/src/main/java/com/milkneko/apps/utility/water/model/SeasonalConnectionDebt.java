c64bb48c1d6174c3cfc32e25c6433f223baf2d7a
Luis Yucra <luis.yucra@gmail.com>
2016-09-24 14:05:43 -0500

    add new attribute

--- server/src/main/java/com/milkneko/apps/utility/water/model/SeasonalConnectionDebt.java
+++ server/src/main/java/com/milkneko/apps/utility/water/model/SeasonalConnectionDebt.java
@@ -11,82 +11,90 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class SeasonalConnectionDebt {
-    @Id
-    @GeneratedValue
-    private int id;
-    private Date issuedDay;
-    private Date dueDate;
+	@Id
+	@GeneratedValue
+	private int id;
+	private Date issuedDay;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Connection connection;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Connection connection;
 
-    @OneToOne(mappedBy = "seasonalConnectionDebt", fetch = FetchType.LAZY)
-    private SeasonalConnectionPayment seasonalConnectionPayment;
+	@OneToOne(mappedBy = "seasonalConnectionDebt", fetch = FetchType.LAZY)
+	private SeasonalConnectionPayment seasonalConnectionPayment;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    private MeasureStamp initialMeasureStamp;
-    @OneToOne(fetch = FetchType.LAZY)
-    private MeasureStamp finalMeasureStamp;
+	@OneToOne(fetch = FetchType.LAZY)
+	private MeasureStamp initialMeasureStamp;
+	@OneToOne(fetch = FetchType.LAZY)
+	private MeasureStamp finalMeasureStamp;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private SeasonEntry seasonEntry;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private SeasonEntry seasonEntry;
+	private Date dueDate;
 
-    public SeasonalConnectionDebt() {
-    }
+	public SeasonalConnectionDebt() {
+	}
 
-    public SeasonalConnectionDebt(Date issuedDay) {
-        this.issuedDay = issuedDay;
-    }
+	public SeasonalConnectionDebt(Date issuedDay) {
+		this.issuedDay = issuedDay;
+	}
 
-    public int getId(){
-        return this.id;
-    }
+	public int getId() {
+		return this.id;
+	}
 
-    public Date getIssuedDay() {
-        return issuedDay;
-    }
+	public Date getIssuedDay() {
+		return issuedDay;
+	}
 
-    public void setIssuedDay(Date issuedDay) {
-        this.issuedDay = issuedDay;
-    }
+	public void setIssuedDay(Date issuedDay) {
+		this.issuedDay = issuedDay;
+	}
 
-    public Connection getConnection() {
-        return connection;
-    }
+	public Connection getConnection() {
+		return connection;
+	}
 
-    public void setConnection(Connection connection) {
-        this.connection = connection;
-    }
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
 
-    public SeasonalConnectionPayment getSeasonalConnectionPayment() {
-        return seasonalConnectionPayment;
-    }
+	public SeasonalConnectionPayment getSeasonalConnectionPayment() {
+		return seasonalConnectionPayment;
+	}
 
-    public void setSeasonalConnectionPayment(SeasonalConnectionPayment seasonalConnectionPayment) {
-        this.seasonalConnectionPayment = seasonalConnectionPayment;
-    }
+	public void setSeasonalConnectionPayment(SeasonalConnectionPayment seasonalConnectionPayment) {
+		this.seasonalConnectionPayment = seasonalConnectionPayment;
+	}
 
-    public MeasureStamp getInitialMeasureStamp() {
-        return initialMeasureStamp;
-    }
+	public MeasureStamp getInitialMeasureStamp() {
+		return initialMeasureStamp;
+	}
 
-    public void setInitialMeasureStamp(MeasureStamp initialMeasureStamp) {
-        this.initialMeasureStamp = initialMeasureStamp;
-    }
+	public void setInitialMeasureStamp(MeasureStamp initialMeasureStamp) {
+		this.initialMeasureStamp = initialMeasureStamp;
+	}
 
-    public MeasureStamp getFinalMeasureStamp() {
-        return finalMeasureStamp;
-    }
+	public MeasureStamp getFinalMeasureStamp() {
+		return finalMeasureStamp;
+	}
 
-    public void setFinalMeasureStamp(MeasureStamp finalMeasureStamp) {
-        this.finalMeasureStamp = finalMeasureStamp;
-    }
+	public void setFinalMeasureStamp(MeasureStamp finalMeasureStamp) {
+		this.finalMeasureStamp = finalMeasureStamp;
+	}
 
-    public SeasonEntry getSeasonEntry() {
-        return seasonEntry;
-    }
+	public SeasonEntry getSeasonEntry() {
+		return seasonEntry;
+	}
 
-    public void setSeasonEntry(SeasonEntry seasonEntry) {
-        this.seasonEntry = seasonEntry;
-    }
+	public void setSeasonEntry(SeasonEntry seasonEntry) {
+		this.seasonEntry = seasonEntry;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date param) {
+		this.dueDate = param;
+	}
 }
